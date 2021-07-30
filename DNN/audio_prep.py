c9c0ae75e2334a58872c587c06d9ae57625120a6
PT <63238027+peteritom@users.noreply.github.com>
2021-07-30 14:20:51 +0200

    Add files via upload

---
+++ DNN/audio_prep.py
@@ -0,0 +1,50 @@
+import librosa, librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+file = "/11- Preprocessing audio data for deep learning_code_blues.00000.wav"
+
+# waveform
+
+signal, sr = librosa.load(file, sr=22050) # sr * T -> 22050 * 30
+
+"""librosa.display.waveplot(signal, sr=sr)
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.show()"""
+
+# FFT
+
+fft = np.fft.fft(signal)
+magnitude = np.abs(fft)
+frequency = np.linspace(0, sr, len(magnitude))
+left_frequency = frequency[:int(len(frequency)/2)]
+left_magnitude = magnitude[:int(len(frequency)/2)]
+"""plt.plot(left_frequency, left_magnitude)
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
+plt.show()"""
+
+# STFT
+
+n_fft = 2048
+#n_fft = 1024
+hop_length = 512
+#hop_length = 256
+
+stft = librosa.core.stft(signal, n_fft=n_fft, hop_length=hop_length)
+spectrogram = np.abs(stft)
+log_spectrogram = librosa.amplitude_to_db(spectrogram)
+"""librosa.display.specshow(log_spectrogram, sr=sr, hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.colorbar()
+plt.show()"""
+
+# MFCCs
+
+MFCCs = librosa.feature.mfcc(signal, n_fft=n_fft, hop_length=hop_length, n_mfcc=13)
+librosa.display.specshow(MFCCs, sr=sr, hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("MFCC")
+plt.colorbar()
+plt.show()
