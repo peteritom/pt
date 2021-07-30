378142f1b70097fadff65b82c55d28516247939b
PT <63238027+peteritom@users.noreply.github.com>
2021-07-30 15:26:08 +0200

    Delete DSP - ISEL-Erasmus_2015 directory

--- DSP - ISEL-Erasmus_2015/gen_sin.m
+++
@@ -1,8 +0,0 @@
-function [ y ] = gen_sin( A, fx, fs, duration )
-
-t = 0:1/fs:duration;
-y = A*sin(2*pi*fx*t);
-
-
-end
-
--- DSP - ISEL-Erasmus_2015/gen_square.m
+++
@@ -1,8 +0,0 @@
-function [ y ] = gen_square( A, fx, fs, duration, duty )
-
-t = 0:1/fs:duration;
-y = A*square(2*pi*fx*t, duty);
-
-
-end
-
